209bc8aa85c9712d9f6e6bea76bf2ce71e1b38c0
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-04-28 15:44:52 -0300

    VALIDACIONES DESABILITADAS

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -18,9 +18,9 @@ const deleteCategory = require("./Categories/deleteCategory");
 const getProductBrand = require("../Filters/getProductBrand");
 const getProductCategory = require("../Filters/getProductCategory.js");
 // end filtros / start validaciones
-const { validatorProduct } = require("../Validators/ValidatorProduct");
+/* const { validatorProduct } = require("../Validators/ValidatorProduct");
 const { validatorUser } = require("../Validators/ValidatorUser");
-const { validatorCategory } = require("../Validators/ValidatorCategory");
+const { validatorCategory } = require("../Validators/ValidatorCategory"); */
 // end validaciones / start shops
 const postShop = require("./Shops/postShop");
 const getShops = require("./Shops/getShops");
@@ -31,17 +31,17 @@ const postCarrusel = require("./Carrusel/postCarrusel");
 
 // start products
 router.use("/products", getProduct, getProductBrand, getProductCategory);
-router.use("/postProduct", validatorProduct, postProduct);
+router.use("/postProduct", /* validatorProduct, */ postProduct);
 router.use("/updateProduct", updateProduct);
 router.use("/deleteProduct", deleteProduct);
 // end products / start Users
 router.use("/users", getUsers, getUserById);
-router.use("/postUser", validatorUser, postUser);
+router.use("/postUser", /* validatorUser, */ postUser);
 router.use("/updateUser", updateUser);
 router.use("/deleteUser", deleteUser);
 // end users / start categories
 router.use("/categories", Categories);
-router.use("/postCategory", validatorCategory, postCategory);
+router.use("/postCategory", /* validatorCategory, */ postCategory);
 router.use("/deleteCategory", deleteCategory);
 // end categories / start shop
 router.use("/postShop", postShop);
