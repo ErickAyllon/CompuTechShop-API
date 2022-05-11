0106c61ea894eed7a61e5fbe271bdc0da7159dd0
Erick Ayllon <erickayllon@gmail.com>
2022-05-11 03:40:10 -0300

    esto se rompe?

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => { // si tenes en true, renueva la base de datos
 
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
-});
+}); 
