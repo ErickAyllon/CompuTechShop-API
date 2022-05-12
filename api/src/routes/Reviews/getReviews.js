892dae643259794a7a3e00dfda5046d889f9abdd
lucasmandirola <95371240+lucasmandirola@users.noreply.github.com>
2022-05-12 13:30:38 -0300

    get review by product name arreglado

--- api/src/routes/Reviews/getReviews.js
+++ api/src/routes/Reviews/getReviews.js
@@ -6,17 +6,17 @@ const { getAllComments, getCommentByUserId, getCommentByProductId, getCommentByP
 router.get('/', async (req,res) => {
   const {userId} = req.query
   const {productId} = req.query
-  const {userName} = req.query
+  const {productName} = req.query
 	try {
-    if(userName){
-      const product = await getCommentByUserId(userName)
+    if(productName){
+      const product = await getCommentByProductName(productName)
       res.send(product)
     }
-    if(userId){
+    else if(userId){
       const product = await getCommentByUserId(userId)
       res.send(product)
     }
-    if(productId){
+    else if(productId){
       const product = await getCommentByProductId(productId)
       res.send(product)
     }
