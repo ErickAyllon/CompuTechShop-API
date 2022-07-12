74810db85c79170ba282a3e72fe0a4efe30fee29
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-07-12 17:17:04 -0300

    Update getOrders.js

--- api/src/routes/Payments/getOrders.js
+++ api/src/routes/Payments/getOrders.js
@@ -4,8 +4,7 @@ const { getOrders,getOrdersEmail } = require("../../Controllers/Payments");
 
 router.get('/', async (req,res) => {
   const {userEmail} = req.query
-  console.log("HOLAAAAAAAAAAAAAAAAAAAAAAA");
-	try {
+  	try {
     if(userEmail){
       const order = await getOrdersEmail(userEmail)
       res.send(order)
